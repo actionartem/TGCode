a0122d35af37333ceee83e7b0ff1608c49128196
Artem <Actionartem@gmail.com>
2026-02-01 16:36:41 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ВАЖНО для GitHub Pages: включает static export в папку /out
+  output: "export",
+
+  // Чтобы роуты корректно работали как статические файлы (/about/index.html)
+  trailingSlash: true,
+
+  // Если сайт будет НЕ на отдельном домене, а на https://user.github.io/repo/
+  // тогда нужно раскомментировать basePath/assetPrefix и указать имя репозитория.
+  // basePath: "/TGCode",
+  // assetPrefix: "/TGCode/",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
